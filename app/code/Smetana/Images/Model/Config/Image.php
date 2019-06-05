6870596f421db21e1742df89c4c992d5cdf2408e
vladyslavsmetana <vl.chubir@gmail.com>
2019-06-05 11:35:44 +0300

    Images Module Frontend Display 444

--- app/code/Smetana/Images/Model/Config/Image.php
+++ app/code/Smetana/Images/Model/Config/Image.php
@@ -16,6 +16,55 @@ class Image extends \Magento\Config\Model\Config\Backend\Image
     const UPLOAD_DIR = 'products_image';
 
     /**
+     * File Operations
+     *
+     * @var \Magento\Framework\Filesystem\Driver\File
+     */
+    private $file;
+
+    /**
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $config
+     * @param \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
+     * @param \Magento\MediaStorage\Model\File\UploaderFactory $uploaderFactory
+     * @param \Magento\Config\Model\Config\Backend\File\RequestData\RequestDataInterface $requestData
+     * @param \Magento\Framework\Filesystem $filesystem
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb $resourceCollection
+     * @param array $data
+     * @param \Magento\Framework\Filesystem\Driver\File $file
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\App\Config\ScopeConfigInterface $config,
+        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
+        \Magento\MediaStorage\Model\File\UploaderFactory $uploaderFactory,
+        \Magento\Config\Model\Config\Backend\File\RequestData\RequestDataInterface $requestData,
+        \Magento\Framework\Filesystem $filesystem,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = [],
+        \Magento\Framework\Filesystem\Driver\File $file = null
+    ) {
+        $this->file = $file
+            ?? ObjectManager::getInstance()->get(\Magento\Framework\Filesystem\Driver\File::class);
+        parent::__construct(
+            $context,
+            $registry,
+            $config,
+            $cacheTypeList,
+            $uploaderFactory,
+            $requestData,
+            $filesystem,
+            $resource,
+            $resourceCollection,
+            $data
+        );
+    }
+
+    /**
      * Returning path to directory for upload file
      *
      * @return string
@@ -37,16 +86,6 @@ class Image extends \Magento\Config\Model\Config\Backend\Image
     }
 
     /**
-     * Getting for allowed extensions of uploaded files
-     *
-     * @return array
-     */
-    protected function _getAllowedExtensions(): array
-    {
-        return ['jpg', 'jpeg'];
-    }
-
-    /**
      * Changing process of saving image
      *
      * @throws \Magento\Framework\Exception\LocalizedException
@@ -54,17 +93,16 @@ class Image extends \Magento\Config\Model\Config\Backend\Image
     public function beforeSave()
     {
         if (!empty($this->getFileData())) {
-            $operations = ObjectManager::getInstance()->create(\Magento\Framework\Filesystem\Driver\File::CLASS);
-            if (!$operations->isReadable($this->_getUploadDir())) {
+            if (!$this->file->isReadable($this->_getUploadDir())) {
                 $folders = ['default', 'resize'];
                 foreach ($folders as $folder) {
-                    $operations->createDirectory($this->_mediaDirectory->getAbsolutePath() . 'products_image/' . $folder);
+                    $this->file->createDirectory($this->_mediaDirectory->getAbsolutePath() . 'products_image/' . $folder);
                 }
             }
-            $files = $operations->readDirectory($this->_getUploadDir());
+            $files = $this->file->readDirectory($this->_getUploadDir());
             if ($files) {
                 foreach ($files as $file) {
-                    $operations->deleteFile($file);
+                    $this->file->deleteFile($file);
                 }
                 if (mime_content_type($this->getFileData()['tmp_name']) != 'image/jpeg') {
                     throw new \Magento\Framework\Exception\LocalizedException(__('%1', 'The file has the wrong extension'));
